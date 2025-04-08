6a8475ea7e4b8b830ab9c2ed5079dc85da8c8e0e
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-09 00:17:25 +0500

    Updating Data

--- app/backend/Controller/productController.js
+++ app/backend/Controller/productController.js
@@ -1,14 +1,19 @@
 
 
-
-exports.getAllProducts=async(req,res,next)=>{
-
-try{
-
-req.status(200)
-
-}catch(err){
-
-}
-
-}
+exports.getAllProducts = async (req,res,next) => {
+    console.log('🤠🤠🤠🤠🤠 getAllProducts controller hit!');
+  
+    try {
+      res.status(200).json({
+        status: 'success',
+        data: [], // you can put your products array here
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong!',
+      });
+    }
+  };
+  
