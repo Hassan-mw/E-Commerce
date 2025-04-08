e644582222418a0612127b01086aad0611a67d50
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-08 18:00:54 +0500

    Updating Data

---
+++ app/backend/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const express=require('express');
+const app=express()
+
+
+app.listen(process.env.PORT ,()=>{
+console.log(`server was runing on port ${process.env.PORT}`)
+})
+
