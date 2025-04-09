655b26170aed94f48897a1c3f428064953a0e589
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-10 01:12:34 +0500

    Updating Data

--- app/backend/server.js
+++ app/backend/server.js
@@ -2,31 +2,22 @@
 const express=require('express');
 const app=express()
 const Pool=require('./Pool/pool')
-app.use(express.json());
 const productRoutes=require('./Router/productRoutes')
 const  AppError=require('./ErrorHandler/appError') 
+app.use(express.json());
 
 
+app.use((req,res,next)=>{
+    console.log(req.body)
+    next()
+})
 
 
 app.use('/api/products',productRoutes);
 
   
 
-// ! GLobal Error Middleware
-// Global error handler for undefined routes (Page Not Found)
-app.use((req, res, next) => {
-    res.status(404).json({
-      status: 'fail',
-      message: `Can't find ${req.originalUrl} on this server`
-    });
-  });
-  
-  
-  app.use((err,req,res,next)=>{
-  
-    res.status(500).send("some thing was broken!")
-  })
+
   
 Pool.connect({
     host:"localhost",
@@ -48,3 +39,17 @@ app.listen(process.env.PORT || 5000 ,()=>{
 console.log(`server was runing on port ${process.env.PORT}`)
 })
 
+// ! GLobal Error Middleware
+// Global error handler for undefined routes (Page Not Found)
+// app.use((req, res, next) => {
+//   res.status(404).json({
+//     status: 'fail',
+//     message: `Can't find ${req.originalUrl} on this server`
+//   });
+// });
+
+
+// app.use((err,req,res,next)=>{
+
+//   res.status(500).send("some thing was broken!")
+// })
