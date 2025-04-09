4d5070815c078b35cb28c0abb088ed99f6df2a06
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-09 12:28:28 +0500

    Updating Data

--- app/backend/Controller/productController.js
+++ app/backend/Controller/productController.js
@@ -5,35 +5,35 @@ const  AppError=require('./../ErrorHandler/appError')
 const express=require('express')
 const app=express()
 
-// const uploadPath=path.join(__dirname,"../../../public")
+const uploadPath=path.join(__dirname,"../../../public")
 
-// const multerStorage=multer.diskStorage({
-//   destination:(req,file,cb)=>{
-//     cb(null,uploadPath)
-//   },
-//   filename:(req,file,cb)=>{
-//     const ext=file.mimetype.split('/')[1];
-//     cb(null,`userr_${Date.now()}.${ext}`)
-//   }
-// })
+const multerStorage=multer.diskStorage({
+  destination:(req,file,cb)=>{
+    cb(null,uploadPath)
+  },
+  filename:(req,file,cb)=>{
+    const ext=file.mimetype.split('/')[1];
+    cb(null,`userr_${Date.now()}.${ext}`)
+  }
+})
 
-// const multerFilter=(req,file,cb)=>{
-//   if(file.mimetype.startsWith('image')){
-//     cb(null,true)
-//   }
-//   else{
-//     cb(new AppError("Not an iage ! please upload an image"),false)
-//   }
-// }
-
-
-// const upload=multer({
-//   storage:multerStorage,
-  // fileFilter:multerFilter
-// })
+const multerFilter=(req,file,cb)=>{
+  if(file.mimetype.startsWith('image')){
+    cb(null,true)
+  }
+  else{
+    cb(new AppError("Not an iage ! please upload an image"),false)
+  }
+}
 
 
-// exports.uploadProductPicture=upload.single('image')
+const upload=multer({
+  storage:multerStorage,
+  fileFilter:multerFilter
+})
+
+
+exports.uploadProductPicture=upload.single('image')
 
 
 exports.getAllProducts = async (req,res,next) => {
@@ -51,10 +51,12 @@ exports.getAllProducts = async (req,res,next) => {
       });
     }
   };
-console.log('╰(*°▽°*)╯^_^^_^')
+
+
   
 exports.createproduct=async(req,res,next)=>{
     try{
+      console.log(req.file)
   const {rows}=await pool.query('INSERT INTO products()')
 
 
@@ -67,17 +69,3 @@ exports.createproduct=async(req,res,next)=>{
   })
  }
 }
-
-
-// ! GLobal Error Middleware
-app.all('*',(req, res, next)=>{
-  res.status(500).send("some thing was broken!")
-
-  // next(new AppError("Can't find this page", 404));
-});
-
-
-app.use((err,req,res,next)=>{
-
-  res.status(500).send("some thing was broken!")
-})
--- app/backend/Router/productRoutes.js
+++ app/backend/Router/productRoutes.js
@@ -6,7 +6,7 @@ const controller=require('./../Controller/productController')
 
 router.route('/')
 .get(controller.getAllProducts)
-.post(controller.createproduct)
+.post(controller.uploadProductPicture,controller.createproduct)
 
 
 
