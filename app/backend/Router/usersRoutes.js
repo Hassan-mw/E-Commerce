adb271d23ba1559ce737f16fc2d808eb9fbe513c
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-16 13:23:59 +0500

    Updating Data

--- app/backend/Router/usersRoutes.js
+++ app/backend/Router/usersRoutes.js
@@ -1,9 +1,9 @@
-const express=require('express');
+const express=require('express')
 const router=express.Router();
 const userController=require('./../Controller/userController')
 
 router.route('/')
-.get(userController.getAllUsers)
+// .get(userController.getAllUsers)
 .post(userController.signUp)
 
 
