0592d66f4ad71dd1acd2b5c6b89fd157aa9299e3
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-16 12:53:29 +0500

    Updating Data

--- app/backend/Controller/userController.js
+++ app/backend/Controller/userController.js
@@ -1,15 +1,37 @@
 
+const jwt=require('jsonwebtoken')
+const pool = require('../Pool/pool')
 
-exports.getAllUserrs=async()=>{
+
+
+
+
+exports.getAllUsers=async()=>{
 try{
 
 }catch(err){
     
 }
 }
-exports.signUp=async()=>{
-try{
 
+
+
+
+exports.signUp=async(req,res,next)=>{
+try{
+    console.log(req.body.name)
+const {name,email,phone}=req.body
+const {photo}=req.file
+console.log(name,email,phone)
+const {rows}=await pool.query(`INSERT INTO users (name,phone,email,photo) VALUES ($1,$2,$3) RETURNING *`,[name,phone,email,photo])
+console.log(rows,'😔😔😓😓😒😒🤤🤤😝😝😜😜')
+
+// const token=jwt.sign({id:rows.id},process.env.JWT_SECRET,{expiresIn:90})
+res.staus(200).json({
+    status:'success',
+    // token,
+    // data:rows
+})
 }catch(err){
 
 }
--- app/backend/Router/usersRoutes.js
+++ app/backend/Router/usersRoutes.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const userController=require('./../Controller/userController')
 
+console.log('first')
 router.route('/')
 .get(userController.getAllUsers)
 .post(userController.signUp)
