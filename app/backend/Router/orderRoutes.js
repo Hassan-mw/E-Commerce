2a8487739b201fc436d95157c3e4eb4928b59ceb
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-18 22:47:31 +0500

    Updating Data

--- app/backend/Router/orderRoutes.js
+++ app/backend/Router/orderRoutes.js
@@ -1,3 +1,9 @@
 const express=require('express')
 const router=express.Router();
 const orderController=require('./../Controller/orderController')
+
+router.route('/')
+.post(orderController.crea)
+
+
+module.exports=router
