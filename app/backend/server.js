2ba0dee50cce90e460188ca1839ff86e9fc49101
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-18 18:39:37 +0500

    Updating Data

--- app/backend/server.js
+++ app/backend/server.js
@@ -17,6 +17,7 @@ app.use('/api/products',productRoutes);
 app.use('/api/users',usersRoutes);
 app.use('/api/reviews',reviewRoutes);
 app.use('/api/favourites',favouriteRoutes);
+app.use('/api/carts',cartRoutes);
 
   
 
