9179be6fb0c88d3fe20912ea5e904e6dccbec5ba
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-19 01:57:08 +0500

    Updating Data

--- app/backend/Router/paymentRoutes.js
+++ app/backend/Router/paymentRoutes.js
@@ -1,3 +1,9 @@
 const express=require('express')
 const router=express.Router();
 const paymentController=require('./../Controller/paymentController')
+
+
+router.route('/')
+.get(paymentController.getAllPaymentData)
+
+module.exports=router
