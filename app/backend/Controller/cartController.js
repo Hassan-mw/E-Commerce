27e97f1f3c2bd22321838c42e25fc005bb4f7392
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-18 22:08:20 +0500

    Updating Data

---
+++ app/backend/Controller/cartController.js
@@ -0,0 +1,22 @@
+
+exports.getAllCart=async(req,resizeBy,next)=>{
+    try{
+
+    }catch(error){
+        resizeBy.status(500).json({
+            status:'fail',
+            message:error.message
+        })
+    }
+}
+
+exports.createCart=async(req,resizeBy,next)=>{
+    try{
+
+    }catch(error){
+        resizeBy.status(500).json({
+            status:'fail',
+            message:error.message
+        })
+    }
+}
