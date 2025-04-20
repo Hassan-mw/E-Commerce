c3568925dc060806f5a79fa1514871f0c5a1f9b9
Hassan <https://github.com/Hassan-mw/SkyScanner.git>
2025-04-20 23:23:16 +0500

    Updating Data

---
+++ app/HomePageComponents/Hero.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const hero = () => {
+  return (
+    <div className='w-full h-full'>
+     <Image fill src="/Home_page.png" alt='Home-Page-Image'/>
+    </div>
+  )
+}
+
+export default hero
