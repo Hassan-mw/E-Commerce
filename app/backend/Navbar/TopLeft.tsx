8fae6fbb459a50e738b21cd0886314497d8efb77
Hassan <https://github.com/Hassan-mw/SkyScanner.git>
2025-04-21 00:41:34 +0500

    Updating Data

--- app/backend/Navbar/TopLeft.tsx
+++ app/backend/Navbar/TopLeft.tsx
@@ -1,10 +1,22 @@
+'use client'
+import { Jost } from 'next/font/google'
 import Image from 'next/image'
 import React from 'react'
 
+
+
+const jost=Jost({
+  weight:['500'],
+  subsets:['latin']
+})
+
 const TopLeft = () => {
   return (
-    <div className='w-full flex items-center justify-center'>
-    <div className='flex items-center justify-center gap-x-3'><Image height={90} width={90} src="/Navbar_logo.png" alt="logo" /><span>Limunea</span></div>
+    <div className='w-full flex items-center justify-center gap-x-6'>
+    <div className='flex items-center justify-center gap-x-3'><Image height={45} width={45} src="/Navbar_logo.png" alt="logo" /><span className={`${jost.className} text-2xl`}>Limunea</span></div>
+    <div className='flex items-center justify-center border border-slate-200 rounded-sm py-1 px-2'>
+      <div><input type='text' className='focus:outline-none placeholder:text-slate-500 placeholder:text-sm ' placeholder='Search Product'/></div>
+    </div>
 </div>
   )
 }
