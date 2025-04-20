3f9bb343040733483417d164780722eafa5fdfa5
Hassan <https://github.com/Hassan-mw/SkyScanner.git>
2025-04-21 00:26:38 +0500

    Updating Data

--- app/backend/Navbar/Navbar.tsx
+++ app/backend/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Top from './Top'
+import Bottom from './Bottom'
 
 const Navbar = () => {
   return (
---
+++ app/backend/Navbar/Top.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Top = () => {
+  return (
+    <div className='w-full flex items-center justify-center'>
+    <div className='w-full flex items-center justify-center'>
+        <div></div>
+    </div>
+    <div className='w-full flex items-center justify-between'>
+
+    </div>
+    </div>
+  )
+}
+
+export default Top
