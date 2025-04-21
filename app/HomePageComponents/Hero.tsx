2955dc8c76fa3f933e7c8710552dece6de8de514
Hassan <https://github.com/Hassan-mw/E-Commerce.git>
2025-04-22 00:10:24 +0500

    Updating Frontend Data

--- app/HomePageComponents/Hero.tsx
+++ app/HomePageComponents/Hero.tsx
@@ -4,10 +4,10 @@ import HeroPageData from '../HeroPageData/HeroPageData'
 
 const Hero = () => {
   return (
-    <div className='w-full h-full'>
-    <div className='absolute w-full h-full'>
-     <Image fill className='object-cover h-full w-full' src="/Home_page.png" alt='Home-Page-Image'/>
-      <div className='fixed top-[40%] left-4'><HeroPageData/></div>
+    <div className='w-full h-full  flex items-center justify-center bg-[#b10000]'>
+    <div className=' h-[70vh] w-full max-w-screen-xl   bg-blue-300 relative'>
+     <Image height={1300} width={1400} className='object-cover lg:object-fill w-full h-full ' src="/Home_page.png" alt='Home-Page-Image'/>
+      <div className='absolute left-5 top-10   border '><HeroPageData/></div>
     </div>
     </div>
   )
