f28e16c69977b55394440cd36dc9e972a11273ea
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-22 01:14:19 +0500

    Updating Frontend Data

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,18 @@
 'use client'
 import { ButtonDataTYpe } from '@/app/Types/dataType'
+import { Jost } from 'next/font/google'
 import Link from 'next/link'
 import React from 'react'
 
+
+const jost=Jost({
+  weight:['500'],
+  subsets:['latin']
+})
+
 const Button = ({style,children,href}:ButtonDataTYpe) => {
   return (
-    <Link href={href} className={${} style}>
+    <Link href={href} className={`${jost.className}  ${style}`}>
         {children}
     </Link>
   )
