9599c039a7d19ff4c8e43b6425e16949263e87d5
Hassan <https://github.com/Hassan-mw/E-Commerce.git>
2025-04-22 00:14:02 +0500

    Updating Frontend Data

--- app/HeroPageData/HeroPageData.tsx
+++ app/HeroPageData/HeroPageData.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
+import { Jost } from 'next/font/google'
+
+const jost=Jost({
+  weight:['500'],
+  subsets:['latin']
+})
+
+
 const HeroPageData = () => {
   return (
-    <div className='w-full sm:w-[500px] md:w-full flex flex-col space-y-5 '>
-      <div  className='text-7xl font-bold'>F urnimal</div>
-      <div  className='text-3xl font-semibold '>Furniture Store Shopify Theme</div>
-      <div  className='text-lg  text-[#66666b]'> A best template for different purposes that creativity, efficiency, and diversity.</div>
+    <div className={`${jost.className} w-full sm:w-[500px] md:w-full flex flex-col space-y-2 `}>
+      <div  className={`${jost.className} text-7xl font-bold`}>Furnimal</div>
+      <div  className={`${jost.className} text-3xl font-semibold `}>Furniture Store Shopify Theme</div>
+      <div  className={`${jost.className} text-md  text-[#66666b]`}> A best template for different purposes that creativity, efficiency, and diversity.</div>
+       
     </div>
   )
  
--- app/HomePageComponents/Hero.tsx
+++ app/HomePageComponents/Hero.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image'
 import React from 'react'
 import HeroPageData from '../HeroPageData/HeroPageData'
 
+
 const Hero = () => {
   return (
-    <div className='w-full h-full  flex items-center justify-center bg-[#b10000]'>
+    <div className='w-full h-full  flex items-center justify-center bg-[#f6f6f6]'>
     <div className=' h-[70vh] w-full max-w-screen-xl   bg-blue-300 relative'>
      <Image height={1300} width={1400} className='object-cover lg:object-fill w-full h-full ' src="/Home_page.png" alt='Home-Page-Image'/>
-      <div className='absolute left-5 top-10   border '><HeroPageData/></div>
+      <div className='absolute left-5 top-10    '><HeroPageData/></div>
     </div>
     </div>
   )
