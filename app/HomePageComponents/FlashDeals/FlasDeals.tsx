b4f0045bfa8d2ce4f4bddfb765a7cf1cc8d512f2
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-22 01:16:48 +0500

    Updating Frontend Data

---
+++ app/HomePageComponents/FlashDeals/FlasDeals.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FlasDeals = () => {
+  return (
+    <div className='w-full flex items-center justify-center'>
+    <div className='w-full max-w-screen-xl bg-red-700 flex items-center justify-center'>
+
+    </div>
+    </div>
+  )
+}
+
+export default FlasDeals
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './HomePageComponents/Hero'
+import FlasDeals from './HomePageComponents/FlashDeals/FlasDeals'
 
 
 const page = () => {
   return (
     <div className='w-full h-full flex flex-col '>
     <Hero/>
+    <FlasDeals/>
     </div>
   )
 }
