f8b3440e36239a6baf9d3706c7761a53f2119aaf
Hassan <https://github.com/Hassan-mw/E-Commerce.git>
2025-04-21 23:24:17 +0500

    Updating Frontend Data

--- app/HeroPageData/HeroPageData.tsx
+++ app/HeroPageData/HeroPageData.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeroPageData = () => {
   return (
     <div className='w-full sm:w-[400px] flex flex-col space-y-5 '>
-      <div  className='text-7xl font-bold'>Furnimal</div>
+      <div  className='text-7xl font-bold'>F urnimal</div>
       <div  className='text-3xl font-semibold '>Furniture Store Shopify Theme</div>
       <div  className='text-lg  text-[#66666b]'> A best template for different purposes that creativity, efficiency, and diversity.</div>
     </div>
