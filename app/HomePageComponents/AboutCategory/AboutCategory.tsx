316edb9a0c68b7ccb6ad71e225c809dac7ff9671
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-22 23:22:43 +0500

    Updating Frontend Data

---
+++ app/HomePageComponents/AboutCategory/AboutCategory.tsx
@@ -0,0 +1,22 @@
+import { Jost } from 'next/font/google'
+import React from 'react'
+import AboutCategoryData from './AboutCategoryData'
+
+const jost=Jost({
+  weight:['500'],
+  subsets:['latin']
+})
+
+
+const AboutCategory = () => {
+  return (
+    <div className='w-full flex items-center justify-center bg-white py-9'>
+    <div className='w-full max-w-screen-xl px-10 flex flex-col items-center justify-center'>
+    
+      <AboutCategoryData/> 
+    </div>
+    </div>
+  )
+}
+
+export default AboutCategory
--- app/HomePageComponents/TopNumbersProduct/TopNumberProductData.tsx
+++ app/HomePageComponents/TopNumbersProduct/TopNumberProductData.tsx
@@ -11,10 +11,10 @@ const jost=Jost({
 
 const TopNUmberProductData = () => {
     const productData=[
-      {id:1,name:"Gaming Chair",details:"Black gaming chair confortable ",rating:36,price:82,image:"top_hundred_photo_1.png"},
-      {id:3,name:"Black Sneakers",details:"Black color sneakers for men",rating:"44",price:55,image:"top_hundred_photo_3.png"},
-      {id:4,name:"Black Sneakers",details:"Black color sneakers for men",rating:"44",price:55,image:"top_hundred_photo_4.png"},
-      {id:2,name:"Black Sneakers",details:"Black color sneakers for men",rating:"44",price:55,image:"top_hundred_photo_2.png"},
+      {id:1,name:"Mango",details:"Kimono & Caftan - Black - Regular fit ",rating:34,price:342,image:"top_hundred_photo_1.png"},
+      {id:3,name:"Iniqlo ",details:"Midi atlas Slim fit - bohemian ",rating:"44",price:234,image:"top_hundred_photo_3.png"},
+      {id:4,name:"Mango dress",details:"Kimono & Caftan - Colorful - Night club fit",rating:222,price:55,image:"top_hundred_photo_4.png"},
+      {id:2,name:"Zara",details:"Midi top- Daily fit",rating:77,price:346,image:"top_hundred_photo_2.png"},
         // {id:1,name:"Gaming Controller",details:"Pink gaming controller for girls",rating:"66",price:56,image:"trending_product_1.png"},
         // {id:4,name:"Gaming Chair",details:"Black gaming chair confortable ",rating:36,price:82,image:"trending_product_3.png"},
     ]
@@ -33,17 +33,17 @@ const TopNUmberProductData = () => {
    <div className='bg-white shadow-2xl border border-slate-100 rounded-b-md  w-full flex flex-col  items-start justify-start space-y-1 p-3 text-white'>
 
     {/* //!  Name */}
-    <div  style={{fontWeight:300}} className={`${jost.className} text-md text-black font-semibold `}>Mango</div>
+    <div  style={{fontWeight:300}} className={`${jost.className} text-md text-black font-semibold `}>{data.name}</div>
     {/* //!  Product_short_detail */}
-    <div  style={{fontWeight:100}} className={`${jost.className}  text-xs text-[#555555] font-semibold `}> Kimono & Caftan -Black Regular fit </div>
+    <div  style={{fontWeight:100}} className={`${jost.className}  text-xs text-[#555555] font-semibold `}> {data.details}</div>
 
 
     {/* //!  Product_Rating */}
-    <div   className={`${jost.className} text-xs text-[#555555] font-semibold flex items-center justify-center gap-x-3 `}><span style={{fontWeight:200}} className='text-xs'>⭐⭐⭐⭐</span><span className='text-thin'>(34)</span> </div>
+    <div   className={`${jost.className} text-xs text-[#555555] font-semibold flex items-center justify-center gap-x-3 `}><span style={{fontWeight:200}} className='text-xs'>⭐⭐⭐⭐</span><span className='text-thin'>({data.rating})</span> </div>
     {/* //!  Priceing */}
     <div className={`flex items-center justify-center gap-x-3`}>
-        <span  style={{fontWeight:500}}  className='text-xs text-red-600'>$228.99</span>
-        <span  style={{fontWeight:400}} className='text-[#555555] text-[12px]'>$54.99</span>
+        <span  style={{fontWeight:500}}  className='text-xs text-red-600'>${data.price}.99</span>
+        <span  style={{fontWeight:400}} className='text-[#555555] text-[12px]'>${data.price*2+9}.99</span>
         <span className=' rounded-xs text-red-500 text-[11px] '>-40%</span>
     
     
