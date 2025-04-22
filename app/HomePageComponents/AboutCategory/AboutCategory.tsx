cbc212350fbc24bee8427dd95ed7dd83aee32f3a
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-23 01:01:33 +0500

    Updating Frontend Data

--- app/HomePageComponents/AboutCategory/AboutCategory.tsx
+++ app/HomePageComponents/AboutCategory/AboutCategory.tsx
@@ -4,14 +4,18 @@ import AboutCategoryData from './AboutCategoryData'
 import MobileShow from './MobileShow'
 
 const AboutCategory = () => {
+  const data=[
+    {id:1,title:'Never-Ending Summer',belowTitle:'Throwback Shirts & all-day dressed',image:'red_dress.png',style:'bg-[#BF2E3B]'},
+    {id:2,title:'Famous sport brandsNever-Ending Summer',belowTitle:'Get in gym essentialsThrowback Shirts & all-day dressed',image:'blue_dress.png',style:'bg-[#1D5159]'}
+  ]
   return (
     <div className='hidden lg:block'>
     <div className='w-full flex items-center justify-center bg-white py-9'>
     <div className='w-full max-w-screen-xl px-10 flex flex-col items-center justify-center space-y-8'>
     
-      <AboutCategoryData /> 
+      <AboutCategoryData data={data} /> 
       <MobileShow/>
-      <AboutCategoryData/> 
+      {/* <AboutCategoryData/>  */}
     </div>
     </div>
     </div>
