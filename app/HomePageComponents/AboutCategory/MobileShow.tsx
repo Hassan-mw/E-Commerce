acbbea10b1dd2edd316503ec8b0631a3c58f035f
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-23 00:48:31 +0500

    Updating Frontend Data

--- app/HomePageComponents/AboutCategory/MobileShow.tsx
+++ app/HomePageComponents/AboutCategory/MobileShow.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+//Iphone
 const MobileShow = () => {
   return (
-    <div>MobileShow</div>
+    <div className='w-full flex items-center justify-between px-20 bg-[#F7DDD0]'>
+     <div>
+        
+     </div>
+    </div>
   )
 }
 
