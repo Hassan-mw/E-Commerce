c49b7f6b35e2a0e40d7182903c115de067930c9f
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-23 00:53:27 +0500

    Updating Frontend Data

--- app/HomePageComponents/AboutCategory/AboutCategory.tsx
+++ app/HomePageComponents/AboutCategory/AboutCategory.tsx
@@ -3,17 +3,11 @@ import React from 'react'
 import AboutCategoryData from './AboutCategoryData'
 import MobileShow from './MobileShow'
 
-const jost=Jost({
-  weight:['500'],
-  subsets:['latin']
-})
-
-
 const AboutCategory = () => {
   return (
     <div className='hidden lg:block'>
     <div className='w-full flex items-center justify-center bg-white py-9'>
-    <div className='w-full max-w-screen-xl px-10 flex flex-col items-center justify-center'>
+    <div className='w-full max-w-screen-xl px-10 flex flex-col items-center justify-center space-y-8'>
     
       <AboutCategoryData /> 
       <MobileShow/>
--- app/HomePageComponents/AboutCategory/MobileShow.tsx
+++ app/HomePageComponents/AboutCategory/MobileShow.tsx
@@ -1,10 +1,23 @@
+import { Jost } from 'next/font/google'
+import Image from 'next/image'
 import React from 'react'
 //Iphone
+
+const jost=Jost({
+    weight:['500'],
+    subsets:['latin']
+  })
+  
+  
 const MobileShow = () => {
   return (
-    <div className='w-full flex items-center justify-between px-20 bg-[#F7DDD0]'>
+    <div className='w-full flex items-center justify-between px-20 bg-[#F7DDD0] rounded-md'>
+     <div className=' flex flex-col items-start justify-start space-y-2'>
+    <div className={`${jost.className} text-2xl text-[#465D6B]`}>MAGSAFE</div>
+    <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>Snap on a magnatic case,wallet, or both.And get fater wireless charging.</div>
+     </div>
      <div>
-        
+        <Image height={333} width={333} className='object-cover h-full w-full' src="/Iphone.png" alt='Iphone'/>
      </div>
     </div>
   )
