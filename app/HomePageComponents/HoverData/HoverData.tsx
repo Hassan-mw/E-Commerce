e9ca67f8eb128034f7c820b8f89c8e8028abd64a
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-24 00:19:10 +0500

    Updating Frontend Data

--- app/HomePageComponents/HoverData/HoverData.tsx
+++ app/HomePageComponents/HoverData/HoverData.tsx
@@ -2,6 +2,11 @@
 
 import * as React from "react"
 import Link from "next/link"
+const jost=Jost({
+  weight:['500'],
+  subsets:['latin']
+})
+
 
 import { cn } from "@/lib/utils"
 // import { Icons } from "@/components/icons"
@@ -14,87 +19,145 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
+import { Jost } from "next/font/google"
 
-const components: { title: string; href: string; description: string }[] = [
+const mens: { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
+    title: "Mans Shose",
     href: "/docs/primitives/alert-dialog",
     description:
       "A modal dialog that interrupts the user with important content and expects a response.",
   },
   {
-    title: "Hover Card",
+    title: "Mans Shirts",
     href: "/docs/primitives/hover-card",
     description:
       "For sighted users to preview content available behind a link.",
   },
   {
-    title: "Progress",
+    title: "Mans Boots",
     href: "/docs/primitives/progress",
     description:
       "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
   },
   {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
+    title: "Mans Jacket",
+    href: "/docs/primitives/progress",
     description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  }
+  
+ 
+
 ]
+const womens: { title: string; href: string; description: string }[] = [
+  {
+    title: "Womans Shose",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Womans Shirts",
+    href: "/docs/primitives/hover-card",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "Womans Boots",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "Womans Jacket",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  }
+  
+ 
+
+]
+const childrens: { title: string; href: string; description: string }[] = [
+  {
+    title: "Childrens Shose",
+    href: "/docs/primitives/alert-dialog",
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "Childrens Shirts",
+    href: "/docs/primitives/hover-card",
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "Childrens Boots",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "Childrens Jacket",
+    href: "/docs/primitives/progress",
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  }
+  
+ 
+
+]
+
 
 const HoverData=()=> {
   return (
-    <NavigationMenu>
+    <div className="w-full flex items-center justify-center">
+
+    <NavigationMenu className="w-screen flex items-center justify-center ">
       <NavigationMenuList>
-        {/* <NavigationMenuItem className="bg-white">
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                   
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components built with Radix UI and
-                      Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem> */}
+        {/* //! 1 */}
+          
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuTrigger className={`${jost.className} hover:cursor-pointer`}>Mans</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] bg-white ">
-              {components.map((component) => (
+              {mens.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        {/* //! 2 */}
+          
+        <NavigationMenuItem>
+        <NavigationMenuTrigger className={`${jost.className}`}>Womens</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] bg-white ">
+              {womens.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        {/* //! 3 */}
+        <NavigationMenuItem>
+        <NavigationMenuTrigger className={`${jost.className}`}>Childrens</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] bg-white ">
+              {childrens.map((component) => (
                 <ListItem
                   key={component.title}
                   title={component.title}
@@ -109,6 +172,7 @@ const HoverData=()=> {
        
       </NavigationMenuList>
     </NavigationMenu>
+    </div>
   )
 }
 
