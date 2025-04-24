4a0105faf48fc656bf2fc41a2c9135cf71c3f11b
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-24 23:03:44 +0500

    Updating Frontend Data

--- app/products/ProductData.tsx
+++ app/products/ProductData.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ProductSideBarTrigger from './ProductSideBarTrigger'
 import TopNUmberProductData from '../HomePageComponents/TopNumbersProduct/TopNumberProductData'
+import ProductDataArray from './ProductDataArray'
 
 const ProductData = () => {
   return (
     <div className='w-full flex flex-col items-center justify-center bg-white py-6'>
      <div>
-        <TopNUmberProductData/>
+        <ProductDataArray/>
     </div>
     </div>
   )
--- app/products/ProductListSidebarData.tsx
+++ app/products/ProductListSidebarData.tsx
@@ -8,6 +8,46 @@ import { IoSearchOutline } from "react-icons/io5";
 import { BiCategory } from "react-icons/bi";
 
 const ProductListSidebarData = () => {
+
+  const modelArray=[
+    {id:1,name:'Shorts',url:'shorts'},
+    {id:2,name:'Mid-length',url:'mid-length'},
+    {id:3,name:'Sweather',url:'sweather'},
+    {id:4,name:'Party Dresses',url:'party-dresses'},
+    {id:5,name:'Requlae Fit',url:'reqular-fit'}
+  ]
+
+
+  const styleArray=[
+    {id:1,name:'Casual',url:'casual'},
+    {id:2,name:'Bussiness Casual',url:'bussiness-casual'},
+    {id:3,name:'Bohemian',url:'bohemian'},
+    {id:4,name:'Zara',url:'zara'},
+    {id:5,name:'Mango',url:'mango'}
+  ]
+
+  const colorArray=[
+    {id:1,name:'green'},
+    {id:2,name:'red'},
+    {id:3,name:'blue'},
+    {id:4,name:'yellow'},
+    {id:5,name:'pink'},
+    {id:6,name:'purple'}
+  ]
+
+  const sizeArray=[
+    {id:1,name:'2XS',url:"2xs"},
+    {id:2,name:'Xs',url:"xs"},
+    {id:3,name:'S',url:"s"},
+    {id:4,name:'M',url:"m"},
+    {id:5,name:'L',url:"l"},
+    {id:6,name:'XL',url:"xl"},
+    {id:7,name:'2XL',url:"2xl"},
+    {id:8,name:'3XL',url:"3xl"}
+  ]
+
+  
+
   return (
     <div className='w-full lg:w-[70%] h-full  flex flex-col items-start justify-start  space-y-12 '> 
     <div className='flex  items-center justify-center gap-x-3 '><div><BiCategory size={22} /></div><div className={`${jost.className} text-xl`}>All Categores</div></div>
@@ -22,23 +62,21 @@ const ProductListSidebarData = () => {
 
     {/* Check box by model */}
     <div className='w-full flex flex-col space-y-2 border-b pb-3 border-slate-300'>
-    <div className={`${jost.className} text-xl`}>Brand</div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Shorts</span></div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Mid-length</span></div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Sweather</span></div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Party Dresses</span></div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Reqular Fit</span></div>
+     <div className={`${jost.className} text-xl`}>Model</div>
+     {
+     modelArray.map((data,index)=>
+    <div key={data.id} className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>{data.name}</span></div>
+    )}
     </div>
 
 
     {/* Check box by Style */}
     <div className='w-full flex flex-col space-y-2 border-b pb-3 border-slate-300'>
     <div className={`${jost.className} text-xl`}>Style</div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Casual</span></div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Business casual</span></div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Bohemian</span></div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Zara</span></div>
-    <div className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>Mango </span></div>
+      {
+      styleArray.map((data,index)=>
+        <div key={data.id} className=' w-full flex items-center justify-start gap-x-4'><input type='checkbox' className='focus:outline-none size-4 placeholder:text-sm' placeholder='Search by brand '  /><span className='tetxt-xl'>{data.name}</span></div>
+       )}
     </div>
 
 
@@ -46,13 +84,11 @@ const ProductListSidebarData = () => {
     <div className='w-full flex flex-col space-y-2 border-b pb-3 border-slate-300'>
     <div className={`${jost.className} text-xl`}>Brand</div>
     <div className='w-full grid grid-cols-4 gap-x-3 gap-y-5'>
-   <div className='size-6 rounded-full bg-green-500'></div>
-   <div className='size-6 rounded-full bg-red-500'></div>
-   <div className='size-6 rounded-full bg-blue-500'></div>
-   <div className='size-6 rounded-full bg-yellow-500'></div>
-   <div className='size-6 rounded-full bg-purple-500'></div>
-   <div className='size-6 rounded-full bg-pink-500'></div>
-   <div className='size-6 rounded-full border border-slate-30 bg-white'></div>
+    { 
+    colorArray.map((data,index)=>
+    <div key={data.id} className={` size-6 rounded-full bg-${data.name}-500`}></div>
+    )}
+   <div className='size-6 rounded-full border border-slate-300 bg-white'></div>
    <div className='size-6 rounded-full bg-black'></div>
     </div>
   
@@ -61,20 +97,15 @@ const ProductListSidebarData = () => {
 
     {/*  box by Size */}
     <div className='w-full flex flex-col space-y-2 border-b pb-3 border-slate-300'>
-    <div className={`${jost.className} text-xl`}>COLOR</div>
+    <div className={`${jost.className} text-xl`}>SIZE</div>
     <div className='w-full grid grid-cols-4 gap-x-3 gap-y-5'>
-<div className={`${jost.className} border border-slate-200 text-[#555555] hover:cursor-pointer hover:bg-[#D1E2EB] duration-200 flex items-center justify-center  px-2 py-1 rounded-sm`}>2XS</div>
-<div className={`${jost.className} border border-slate-200 text-[#555555] flex items-center justify-center  px-2  py-1  rounded-sm`}>XS</div>
-<div className={`${jost.className} border border-slate-200 text-[#555555] flex items-center justify-center  px-2 py-1  rounded-sm`}>S</div>
-<div className={`${jost.className} border border-slate-200 text-[#555555] flex items-center justify-center  px-2 py-1  rounded-sm`}>M</div>
-<div className={`${jost.className} border border-slate-200 text-[#555555] flex items-center justify-center  px-2 py-1  rounded-sm`}>L</div>
-<div className={`${jost.className} border border-slate-200 text-[#555555] flex items-center justify-center  px-2  py-1 rounded-sm`}>XL</div>
-<div className={`${jost.className} border border-slate-200 text-[#555555] flex items-center justify-center  px-2  py-1 rounded-sm`}>2XL</div>
-<div className={`${jost.className} border border-slate-200 text-[#555555] flex items-center justify-center  px-2 py-1  rounded-sm`}>3XL</div>
-   
-    </div>
-  
-    </div>
+      {
+       sizeArray.map((data,index)=>
+      <div key={data.id} className={`${jost.className} border border-slate-200 text-[#555555] hover:cursor-pointer hover:bg-[#D1E2EB] duration-200 flex items-center justify-center  px-2 py-1 rounded-sm`}>{data.name}</div>
+       )
+      }
+ 
+    </div></div>
 
 
     {/* Price */}
