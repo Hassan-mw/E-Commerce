0eab7994c4d96d69c72307e5b95d53fce23f5115
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-24 12:10:41 +0500

    Updating Frontend Data

--- app/HomePageComponents/AboutCategory/ScrollProductShow.tsx
+++ app/HomePageComponents/AboutCategory/ScrollProductShow.tsx
@@ -20,24 +20,51 @@ const jost=Jost({
 const ScrollProductShow = () => {
   return (
     // <div className='w-full flex items-center justify-between px-20 bg-[#F7DDD0] rounded-md'>
-      <Carousel className='   '>
-  <CarouselContent className=' max-h-80  gap-x-4 rounded-md'>
-    <CarouselItem  className=' w-full flex items-center justify-between px-30 bg-[#F7DDD0] rounded-md'>
-     <div className=' flex flex-col items-start justify-start space-y-2'>
+      <Carousel className=' '>
+  <CarouselContent className=' max-h-80  gap-x-4 rounded-md max-w-screen-xl '>
+    {/* //!  1 */}
+    <CarouselItem  className=' w-full flex items-center justify-between px-20 bg-[#F7DDD0] rounded-md'>
+     <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
     <div className={`${jost.className} text-2xl text-[#31373a]`}>MAGSAFE</div>
     <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>Snap on a magnatic case,wallet, or both.And get fater wireless charging.</div>
      </div>
      <div>
-        <Image height={333} width={333} className='object-cover h-full w-full' src="/Iphone.png" alt='Iphone'/>
+        <Image height={333} width={333} className='object-cover h-full w-full max-h-44' src="/Iphone.png" alt='Iphone'/>
      </div>
      </CarouselItem>
-    <CarouselItem  className=' w-full flex items-center justify-between px-30 bg-[#F7DDD0] '>
-     <div className=' flex flex-col items-start justify-start space-y-2'>
+    {/* //!  2 */}
+
+    <CarouselItem  className=' w-full flex items-center justify-between px-20 bg-[#F7DDD0] '>
+     <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
     <div className={`${jost.className} text-2xl text-[#31373a]`}>Reebox</div>
     <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>Flexible sole, appropriate tread for the function, and ability to absorb impact.</div>
      </div>
      <div>
-        <Image height={333} width={333} className='object-cover h-full w-full' src="/sneakers_set.png" alt='Iphone'/>
+        <Image height={333} width={333} className='object-cover h-full w-full max-h-44' src="/sneakers_set.png" alt='Iphone'/>
+     </div>
+     </CarouselItem>
+    
+    {/* //!  3 */}
+
+    <CarouselItem  className=' w-full flex items-center justify-between px-20 bg-[#F7DDD0] '>
+     <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
+    <div className={`${jost.className} text-2xl text-[#31373a]`}>Reebox</div>
+    <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>Flexible sole, appropriate tread for the function, and ability to absorb impact.</div>
+     </div>
+     <div>
+        <Image height={333} width={333} className='object-cover h-full w-full max-h-44' src="/sneakers_set.png" alt='Iphone'/>
+     </div>
+     </CarouselItem>
+    
+    {/* //!  4 */}
+
+    <CarouselItem  className=' w-full flex items-center justify-between px-20 bg-[#F7DDD0] '>
+     <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
+    <div className={`${jost.className} text-2xl text-[#31373a]`}>CAROTE</div>
+    <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>CAROTE Nonstick Pots and Pans,Cookware Set 16pcs Kitchen Cooking Sets, Pot and Pan Non Stick w/Frying pan.</div>
+     </div>
+     <div>
+        <Image height={333} width={333} className='object-cover h-full w-full max-h-44' src="/cooking_set.jpg" alt='Iphone'/>
      </div>
      </CarouselItem>
     
