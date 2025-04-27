faa95d6e8d36b9aef4a043a0551b5fc0aa325b9a
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-27 16:15:42 +0500

    Updating Frontend Data

--- app/products/ProductArrray.tsx
+++ app/products/ProductArrray.tsx
@@ -23,7 +23,7 @@ const ProductArrray = ({data}:TopNumberProductDataType) => {
        
     // ]
   return (
-    <div className={`  w-full grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3    sm:gap-7 lg:gap-x-14 py-9 sm:px-2`}>
+    <div className={`  w-full grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4   sm:gap-7 lg:gap-x-14 py-9 sm:px-2`}>
         {
 
         data.map((data,index:number)=>
