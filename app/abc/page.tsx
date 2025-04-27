4db3f3d1da77f0507d3463749721a356a16787ae
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-27 12:50:54 +0500

    Updating Frontend Data

--- app/abc/page.tsx
+++ app/abc/page.tsx
@@ -3,15 +3,18 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full items-center justify-center'>
+    <div className='w-full flex items-center justify-center'>
+        <div className='w-full h-full flex items-center justify-center max-w-screen-lg'>
         <div className='w-full h-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-12 p-24 border'>
 
+        <Image height={200} width={200} src={`/product_1745603108048.jpg`} className='object-cover max-h-[400px] w-full h-full  shadow-2xl border border-slate-200 object-top rounded-t-md' alt='product_image' />
+        <Image height={200} width={200} src={`/product_1745606831633.jpg`} className='object-cover max-h-[400px] w-full h-full  shadow-2xl border border-slate-200 object-top rounded-t-md' alt='product_image' />
         <Image height={200} width={200} src={`/product_1745603506140-1.jpeg`} className='object-cover w-full h-full  shadow-2xl border border-slate-200 object-top rounded-t-md' alt='product_image' />
         <Image height={200} width={200} src={`/product_1745603506140-1.jpeg`} className='object-cover w-full h-full  shadow-2xl border border-slate-200 object-top rounded-t-md' alt='product_image' />
-        <Image height={200} width={200} src={`/product_1745603506140-1.jpeg`} className='object-cover w-full h-full  shadow-2xl border border-slate-200 object-top rounded-t-md' alt='product_image' />
-        <Image height={200} width={200} src={`/product_1745603506140-1.jpeg`} className='object-cover w-full h-full  shadow-2xl border border-slate-200 object-top rounded-t-md' alt='product_image' />
+        {/* <Image height={200} width={200} src={`/product_1745603506140-1.jpeg`} className='object-cover w-full h-full  shadow-2xl border border-slate-200 object-top rounded-t-md' alt='product_image' /> */}
         </div>
         
+    </div>
     </div>
   )
 }
--- app/products/ProductArrray.tsx
+++ app/products/ProductArrray.tsx
@@ -33,9 +33,9 @@ const ProductArrray = ({data}:TopNumberProductDataType) => {
 
     {/* //! IMAGE */}
 
-<div className='w-full h-full px-12   max-h-96 md:max-h-[400px] '>
+<div className='w-full h-full  '>
 
-<Image height={400} width={400} src={`/${data.main_image}`} className='object-cover object-top w-full h-full  rounded-t-md' alt='product_image' />
+<Image height={400} width={400} src={`/${data.main_image}`} className='aspect-square object-cover w-full h-full  object-top rounded-t-md' alt='product_image' />
 {/* <Image height={800} width={800} src={`/${data.main_image}`} className='object-cover object-top aspect-auto  w-full h-full max-h-96 md:max-h-64 lg:max-g-96 xl:max-h-[400px] shadow-2xl border border-slate-200 object-top rounded-t-md' alt='product_image' /> */}
 </div>
    <div className=' shadow-2xl h-36 bg-white border border-slate-100 rounded-b-md  w-full flex  sm:flex-col md:flex-row md:gap-x-3 lg:gap-x-0 lg:flex-col items-center justify-between sm:justify-start sm:items-start md:justify-between lg:justify-start space-y-1 p-3 text-white'>
