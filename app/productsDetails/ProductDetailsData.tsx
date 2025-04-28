f96ff38d5cf02062ebb6f7d896e5b72187d0c6b6
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-29 00:26:01 +0500

    Updating Frontend Data

--- app/productsDetails/ProductDetailsData.tsx
+++ app/productsDetails/ProductDetailsData.tsx
@@ -6,7 +6,8 @@ import { ProductDetailsDataType } from '../Types/dataType'
 const ProductDetailsData = ({images,name,price,color,quantity,size}:ProductDetailsDataType) => {
   return (
     <div className='w-full flex flex-col space-y-4'>
-     <div className='w-full grid grid-cols-1 sm:grid-cols-2 gap-x-2'>
+     {/* <div className='w-full grid grid-cols-1 sm:grid-cols-2 gap-x-2'> */}
+     <div className='w-full flex flex-col sm:flex-row'>
       <ImageHandler images={images}/> 
       <TopDataHandler images={images} price={price} name={name} color={color} quantity={quantity} size={size}  /> 
      </div>
