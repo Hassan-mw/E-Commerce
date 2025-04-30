6eb9d901a0b736a2363939575d376ef429c0e0f3
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-30 23:33:31 +0500

    Updating Frontend Data

--- app/backend/server.js
+++ app/backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 
 app.use('/api/random',randomRoutes);
-app.use('/api/getSiilarProduct',randomRoutes);
+app.use('/api/getSiilarProduct',similarRoutes);
 app.use('/api/products',productRoutes);
 app.use('/api/users',usersRoutes);
 app.use('/api/reviews',reviewRoutes);
