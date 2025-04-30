6a6d22231fdeca232fb70664a6cfd4931243d76b
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-01 01:08:50 +0500

    Updating Frontend Data

--- app/productsDetails/ReviewDetail.tsx
+++ app/productsDetails/ReviewDetail.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TotalReviews from './TotalReviews'
 
 const ReviewDetail = () => {
   return (
---
+++ app/productsDetails/TotalReviews.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TotalReviews = () => {
+  return (
+    <div>TotalReviews</div>
+  )
+}
+
+export default TotalReviews
