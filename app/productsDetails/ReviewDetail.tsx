927c1574ea74216c97cc1ef9f3f88e371b286159
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-02 23:53:23 +0500

    Updating Frontend Data

--- app/productsDetails/ReviewDetail.tsx
+++ app/productsDetails/ReviewDetail.tsx
@@ -4,7 +4,7 @@ import AllReviewsColoums from './AllReviewsColoums'
 
 const ReviewDetail = () => {
   return (
-    <div className='w-full flex flex-col space-y-8'>
+    <div className='w-full flex flex-col space-y-14 p5-6'>
       <TotalReviews/>
        <AllReviewsColoums/>
 
