092d361945f13d9dbb817950d8bdec46b28c8eb0
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-02 23:25:06 +0500

    Updating Frontend Data

--- app/productsDetails/AllReviewsColoums.tsx
+++ app/productsDetails/AllReviewsColoums.tsx
@@ -8,21 +8,28 @@ const jost=Jost({
 })
 const AllReviewsColoums = () => {
   const reviewData=[
-    {id:1,image:"user_1744831856658.jpeg",name:"Ryan", comment:""}
+    {id:1,image:"user_1744831856658.jpeg",name:"Ryan", comment:"Good items i love that one"},
+    {id:2,image:"user_1744831788917.jpeg",name:"Sam", comment:"I love this brand cloths.These are reliable,and they sustain for long term."},
+    {id:3,image:"user_1744831788917.jpeg",name:"Sam", comment:"I love this brand cloths.These are reliable,and they sustain for long term."},
+    {id:4,image:"user_1744831788917.jpeg",name:"Sam", comment:"I love this brand cloths.These are reliable,and they sustain for long term."},
   ]
   return (
     <div className='w-full flex flex-col space-y-9 '>
-      {/* 1 */}
-          <div className='flex flex-col space-y-3'>
+      {
+        reviewData.map((data,index)=>
+     
+          <div key={data.id} className='flex flex-col space-y-3'>
             <div className='flex items-center justify-start gap-x-4'>
               {/* image */}
-          <Image height={200} width={200} className='rounded-full size-12 object-cover' src="/user_1744831569443.jpeg" alt='Image' />
+          <Image height={300} width={300} className='rounded-full size-11 object-cover' src={`/${data.image}`} alt='Image' />
               {/* Name */}
-              <div className={`${jost.className} text-2xl`}>Hassan</div>
+              <div className={`${jost.className} text-2xl text-[39D9D9D]`}>{data.name}</div>
             </div>
             {/* Review */}
-            <div>The fabric used in this clothing line is of superior quality, providing a luxurious feel against the skin. It not only looks stunning but also feels incredibly comfortable to wear. The attention to detail in every stitch and seam is evident, making each piece a work of art.</div>
+            <div>{data.comment}</div>
           </div>
+             )
+      }
     </div>
   )
 }
--- app/productsDetails/TotalReviews.tsx
+++ app/productsDetails/TotalReviews.tsx
@@ -11,7 +11,7 @@ const jost=Jost({
 const TotalReviews = () => {
   return (
     <div className='w-full  flex  items-center justify-center'>
-    <div className='w-full max-w-2xl flex gap-x-5'>
+    <div className='w-full max-w-2xl flex flex-col gap-y-6 md:ap-x-5 p-5'>
         {/* review-text */}
         <div className={`w-full flex flex-col items-start justify-start gap-y-1 space-y-2`}>
         <div className={`${jost.className} text-xl `}> Total review rate  </div>
