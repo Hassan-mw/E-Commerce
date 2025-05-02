efc8c55d6bcfdc104ba09310b378bedf7cb3d8df
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-02 15:51:04 +0500

    Updating Frontend Data

--- app/productsDetails/RatingProgress.tsx
+++ app/productsDetails/RatingProgress.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const RatingProgress = ({precent}:{precent:string}) => {
   return (
-    <div className='w-full bg-[#D9D9D9] h-3 rounded-md'> 
-    <div className={`${precent} bg-[#4172DC] h-3 rounded-md`}> 
+    <div className='w-full bg-[#D9D9D9] h-2 rounded-md'> 
+    <div className={`${precent} bg-[#4172DC] h-2 rounded-md`}> 
      
     </div>
     </div>
--- app/productsDetails/TotalReviews.tsx
+++ app/productsDetails/TotalReviews.tsx
@@ -10,23 +10,56 @@ const jost=Jost({
 
 const TotalReviews = () => {
   return (
-    <div className='w-full flex  space-y-5'>
+    <div className='w-full  flex  items-center justify-center'>
+    <div className='w-full max-w-2xl flex gap-x-5'>
         {/* review-text */}
-        <div className={`flex flex-col space-y-2`}>
+        <div className={`w-full flex flex-col space-y-2`}>
         <div className={`${jost.className} text-xl `}> Total review rate  </div>
-        <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#555555] `}>Average rating (10 Reviews & 125 ratings) </div>
+        <div style={{fontWeight:400}} className={`${jost.className} text-sm text-[#555555] `}>Average rating (10 Reviews & 125 ratings) </div>
          <div  style={{fontWeight:300}}  className='flex items-center justify-start gap-x-5'><span>⭐⭐⭐</span><span className={`${jost.className} text-md text-[#555555] `}>4.8/5</span></div>
         </div>
         {/* review-radio */}
         <div className='w-full flex flex-col space-y-2'>
+          {/* 1 */}
        <div className='  flex items-center justify-center space-x-2'>
-        <p className={`${jost.className} text-sm`}>5 Stars</p>
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>5</span> <span>Stars</span></div>
         <RatingProgress precent="w-[20%]" />
-        <div className='bg-slate-500'><Progress className='bg-green-500 w-[66%]' value={44} /></div>
-        <p className={`${jost.className} text-sm`}>74 %</p>
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>75</span> <span>%</span></div>
+
         </div>
+          {/* 2 */}
+       <div className='  flex items-center justify-center space-x-2'>
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>4</span> <span>Stars</span></div>
+        <RatingProgress precent="w-[90%]" />
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>90</span> <span>%</span></div>
+
+        </div>
+          {/* 3 */}
+       <div className='  flex items-center justify-center space-x-2'>
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>3</span> <span>Stars</span></div>
+        <RatingProgress precent="w-[35%]" />
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>35</span> <span>%</span></div>
+
+        </div>
+          {/* 4 */}
+       <div className='  flex items-center justify-center space-x-2'>
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>2</span> <span>Stars</span></div>
+        <RatingProgress precent="w-[44%]" />
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>44</span> <span>%</span></div>
+
+        </div>
+          {/* 5 */}
+       <div className='  flex items-center justify-center space-x-2'>
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>5</span> <span>Stars</span></div>
+        <RatingProgress precent="w-[20%]" />
+        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>75</span> <span>%</span></div>
+
+        </div>
+
+
         </div>
         
+    </div>
     </div>
   )
 }
