ec3e0bd770baa9c5038931f4cdd7bef46162b92f
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-03 00:58:15 +0500

    Updating Frontend Data

--- app/productsDetails/RatingProgress.tsx
+++ app/productsDetails/RatingProgress.tsx
@@ -1,6 +1,8 @@
+import { Percent } from 'lucide-react'
 import React from 'react'
 
 const RatingProgress = ({precent}:{precent:string}) => {
+  console.log(Percent,'.saag')
   return (
     <div className='w-full bg-[#D9D9D9] h-2 rounded-md'> 
     <div className={`${precent} bg-[#4172DC] h-2 rounded-md`}> 
