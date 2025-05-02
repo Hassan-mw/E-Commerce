e39e4429150b104d1e64b2640d26e709922fbff3
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-03 01:41:52 +0500

    Updating Frontend Data

--- app/productsDetails/RatingProgress.tsx
+++ app/productsDetails/RatingProgress.tsx
@@ -1,11 +1,9 @@
-
 import React from 'react'
 
 const RatingProgress = ({precent}:{precent:string}) => {
-  console.log(precent,'.saag')
   return (
-    <div className='w-full bg-[#D9D9D9] h-2 rounded-md'> 
-    <div className={`w-[${precent}] bg-[#4172DC] h-2 rounded-md`}> 
+    <div className='w-full bg-[#D9D9D9] h-3 rounded-md'> 
+    <div className={`w-[${precent}%] bg-[#4172DC] h-3 rounded-md`}> 
      
     </div>
     </div>
--- app/productsDetails/TotalReviews.tsx
+++ app/productsDetails/TotalReviews.tsx
@@ -11,12 +11,11 @@ const jost=Jost({
 const TotalReviews = () => {
    const dataReviews=
     {totalReviews:23,fiveStares:8,fourStar:5,threeStar:2,twoStar:5,oneStar:3}
-    const fiveStarRating=((dataReviews.fiveStares/dataReviews.totalReviews)*100).toFixed(0);
-    const fourStarRating=((12/dataReviews.totalReviews)*100).toFixed(0);
-    const threeStarRating=((dataReviews.threeStar/dataReviews.totalReviews)*100).toFixed(0);
-    const twoStarRating=((dataReviews.twoStar/dataReviews.totalReviews)*100).toFixed(0);
-    const oneStarRating=((dataReviews.oneStar/dataReviews.totalReviews)*100).toFixed(0);
-    console.log(fiveStarRating,fourStarRating,threeStarRating,twoStarRating,oneStarRating)
+    const fiveStarRating=((dataReviews.fiveStares/dataReviews.totalReviews)*100).toFixed(0)
+    const fourStarRating=((dataReviews.fourStar/dataReviews.totalReviews)*100).toFixed(0)
+    const threeStarRating=((dataReviews.threeStar/dataReviews.totalReviews)*100).toFixed(0)
+    const twoStarRating=((dataReviews.twoStar/dataReviews.totalReviews)*100).toFixed(0)
+    const oneStarRating=((dataReviews.oneStar/dataReviews.totalReviews)*100).toFixed(0)
     const sumReviews=dataReviews.fiveStares+dataReviews.fourStar+dataReviews.threeStar+dataReviews.twoStar+dataReviews.oneStar
     const avgRating=(dataReviews.totalReviews/5)
   return (
@@ -25,7 +24,7 @@ const TotalReviews = () => {
         {/* review-text */}
         <div className={`w-full flex flex-col items-start justify-start  gap-y-1 space-y-2`}>
         <div className={`${jost.className} text-xl `}> Total review rate  </div>
-        <div style={{fontWeight:400}} className={`${jost.className} text-sm text-[#555555] `}>Average rating ({avgRating} Reviews & {sumReviews} ratings ) </div>
+        <div style={{fontWeight:400}} className={`${jost.className} text-sm text-[#555555] `}>Average rating ({avgRating} Reviews & {sumReviews} ratings) </div>
          <div  style={{fontWeight:300}}  className='flex items-center justify-start gap-x-5'><span>⭐⭐⭐</span><span className={`${jost.className} text-md text-[#555555] `}>4.8/5</span></div>
         </div>
         {/* review-radio */}
@@ -33,42 +32,35 @@ const TotalReviews = () => {
           {/* 1 */}
        <div className='  flex items-center justify-center space-x-2'>
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>5</span> <span>Stars</span></div>
-        <RatingProgress precent={fiveStarRating} />
+        <RatingProgress precent={`w-[${fiveStarRating}%]`} />
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>{fiveStarRating}</span> <span>%</span></div>
 
         </div>
           {/* 2 */}
        <div className='  flex items-center justify-center space-x-2'>
-        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>5</span> <span>Stars</span></div>
-        <RatingProgress precent={`w-[${44}%]`} />
-        <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>{fourStarRating}</span> <span>%</span></div>
-
-        </div>
-          {/* 2 */}
-       <div className='  flex items-center justify-center space-x-2'>
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>4</span> <span>Stars</span></div>
-        <RatingProgress  precent={fourStarRating} />
+        <RatingProgress  precent={`w-[${fourStarRating}%]`} />
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>{fourStarRating}</span> <span>%</span></div>
 
         </div>
           {/* 3 */}
        <div className='  flex items-center justify-center space-x-2'>
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>3</span> <span>Stars</span></div>
-        <RatingProgress  precent={threeStarRating} />
+        <RatingProgress  precent={`w-[${threeStarRating}%]`} />
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>{threeStarRating}</span> <span>%</span></div>
 
         </div>
           {/* 4 */}
        <div className='  flex items-center justify-center space-x-2'>
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>1</span> <span>Stars</span></div>
-        <RatingProgress  precent={twoStarRating}/>
+        <RatingProgress  precent={`w-[${twoStarRating}%]`}/>
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>{twoStarRating}</span> <span>%</span></div>
 
         </div>
           {/* 5 */}
        <div className='  flex items-center justify-center space-x-2'>
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>5</span> <span>Stars</span></div>
-        <RatingProgress  precent={oneStarRating} />
+        <RatingProgress  precent={`w-[${oneStarRating}%]`} />
         <div className={`${jost.className} flex items-center justify-center gap-x-1 text-sm`}><span>{oneStarRating}</span> <span>%</span></div>
 
         </div>
