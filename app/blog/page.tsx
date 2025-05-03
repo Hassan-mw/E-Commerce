23fc1abbacd464e43fd27eb12eeacffd81f32816
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-04 01:24:23 +0500

    Updating Frontend Data

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full flex items-center justify-center'>
-     <Image fill src="blog_main_image.png" alt="blog_main_image" />  
+    <div className='w-full bg-[#9a84b7]  flex items-center justify-center'>
+     <Image height={1000} width={1000} className='object-fill w-full max-w-[1000px]' src="/blog_image_lg.png" alt="blog_main_image" />  
+     {/* <Image height={1000} width={1000} className='object-fill  max-h-[500px]' src="/blog_main_image.png" alt="blog_main_image" />   */}
 
     </div>
   )
