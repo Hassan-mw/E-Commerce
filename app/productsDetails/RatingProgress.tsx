32c1e79fe4d3d37f6b914ee9affd839a6e0ff3be
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-03 11:00:46 +0500

    Updating Frontend Data

--- app/productsDetails/RatingProgress.tsx
+++ app/productsDetails/RatingProgress.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 const RatingProgress = ({precent}:{precent:string}) => {
-  console.log((precent))
-  let x='12%';
+  console.log(precent)
   return (
     <div className='w-full bg-[#D9D9D9] h-3 rounded-md'> 
-    <div className={`max-w-[${(x)}]  bg-[#4172DC] h-3 rounded-md`}> 
+    <div className={`w-[${(precent)}] bg-[#4172DC] h-3 rounded-md`}> 
      
     </div>
     </div>
