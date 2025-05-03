990a8d680a96f2c62919ca757aa8e1345edea307
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-04 01:06:28 +0500

    Updating Frontend Data

--- app/Navbar/TopRight.tsx
+++ app/Navbar/TopRight.tsx
@@ -3,6 +3,7 @@ import { Jost } from 'next/font/google'
 
 import { RiInstagramFill } from "react-icons/ri";
 import { FaFacebookF, FaTwitter } from "react-icons/fa6";
+import Link from 'next/link';
 
 const jost=Jost({
   weight:['500'],
@@ -14,7 +15,7 @@ const TopRight = () => {
     <div className='w-full flex items-center justify-end md:justify-between gap-x-16 md:pb-3'>
      <div style={{fontWeight:300}} className={`${jost.className}  xl:w-[80%]  flex items-center justify-between gap-x-8 text-md text-[#555555]`}>
     <div className='hover:cursor-pointer'>About us</div>
-    <div className='hover:cursor-pointer'>Blog</div>
+    <Link href='/blog'   className='hover:cursor-pointer'>Blog</Link>
     <div className='hover:cursor-pointer'>Contact us</div>
     <div className='hover:cursor-pointer'>Help & support</div>
      </div>
@@ -23,7 +24,7 @@ const TopRight = () => {
  <span className='hover:cursor-pointer  hover:text-blue-400 duration-500'>  <RiInstagramFill /></span>
  <span className='hover:cursor-pointer  hover:text-blue-400 duration-500'>   <FaTwitter /></span>
      </div>
-</div>
+</div>blog_main_image
   )
 }
 
