7cfd161eea0e285ca9a3e38e823e67f898b32d6b
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-03 11:18:45 +0500

    Updating Frontend Data

--- app/productsDetails/AllReviewsColoums.tsx
+++ app/productsDetails/AllReviewsColoums.tsx
@@ -11,8 +11,8 @@ const AllReviewsColoums = () => {
   const reviewData=[
     {id:1,image:"user_1744831856658.jpeg",name:"Ryan", comment:"Good items i love that one",like:'10',disLikes:'1'},
     {id:2,image:"user_1744831788917.jpeg",name:"Sam", comment:"I love this brand cloths.These are reliable,and they sustain for long term.",like:'1',disLikes:'1'},
-    {id:3,image:"user_1744831788917.jpeg",name:"Sam", comment:"I love this brand cloths.These are reliable,and they sustain for long term.",like:'5',disLikes:'2'},
-    {id:4,image:"user_1744831788917.jpeg",name:"Sam", comment:"I love this brand cloths.These are reliable,and they sustain for long term.",like:'2',disLikes:'0'},
+    {id:3,image:"user_1744831788917.jpeg",name:"Sam", comment:"I love this brand cloths.These are reliable,and they sustain for long tssgsdgs sdgsdg sat trgs gsd gsdgsgsg  gsdgerm.",like:'5',disLikes:'2'},
+    {id:4,image:"person_1.jpeg",name:"Sam", comment:"I love this brand cloths.These are reliable,and they sustain for long term.",like:'2',disLikes:'0'},
   ]
   return (
     <div className='w-full flex items-center justify-center'>
@@ -21,17 +21,20 @@ const AllReviewsColoums = () => {
         reviewData.map((data,index)=>
      
           <div key={data.id} className='flex flex-col space-y-3'>
-            <div className='flex items-center justify-between '>
-            <div className='flex items-center justify-start gap-x-4'>
+            <div className='flex items-start justify-between '>
+            <div className='flex items-start justify-start gap-x-4 '>
               {/* image */}
-              <Image height={200} width={200} className='rounded-full size-14 object-cover' src={`/${data.image}`} alt='Image' />
+              <img className='rounded-full size-10  object-contain b' src={`/${data.image}`} alt='Image' />
+              <div className='flex flex-col items-start justify-start '>
               {/* Name */}
-              <div className={`${jost.className} text-2xl text-[39D9D9D]`}>{data.name}</div>
+              <div className={`${jost.className} text-2xl text-[39D9D9D] pb-2`}>{data.name}</div>
+              {/* Review */}
+              <div>{data.comment}</div>
+              </div>
             </div>
-            <LikeDislikeIcons like={data.like} disLike={data.disLikes}  />
+            <div className=''> <LikeDislikeIcons like={data.like} disLike={data.disLikes}  /></div> 
             </div>
-            {/* Review */}
-            <div>{data.comment}</div>
+           
           </div>
              )
       }
