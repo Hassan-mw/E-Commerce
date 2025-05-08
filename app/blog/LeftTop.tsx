26cd7e5859abd92473282535e8037a7b3a9c7aaa
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-09 01:41:47 +0500

    Updating Frontend Data

--- app/blog/LeftTop.tsx
+++ app/blog/LeftTop.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import TopLeft from './TopLeft'
 import TopRight from './TopRight'
 
+
+
 const LeftTop = () => {
   return (
     <div className='w-full grid grid-cols-4 gap-x-1 '>
--- app/blog/TextData.tsx
+++ app/blog/TextData.tsx
@@ -1,8 +1,19 @@
+import { Jost } from 'next/font/google'
 import React from 'react'
+
+const jost=Jost({
+  weight:['500'],
+  subsets:['latin']
+})
+
 
 const TextData = () => {
   return (
-    <div>TextData</div>
+    <div className='w-full flex  items-center justify-center'>
+    <div className='w-full max-w-screen-lg flex flex-col space-y-4'>
+     <div className={`  text-2xl `}>"Unveiling Timeless Elegance: Exploring the Allure of Vintage Fashion"</div>
+    </div>
+    </div>
   )
 }
 
