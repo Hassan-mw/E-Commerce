84f8087c951111446b12ecba41d3c717cb2d6e7f
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-09 00:03:32 +0500

    Updating Frontend Data

--- app/blog/TopLeft.tsx
+++ app/blog/TopLeft.tsx
@@ -11,10 +11,11 @@ const jost=Jost({
 const TopLeft = () => {
   return (
     <div className='  w-full grid grid-cols-5 sm:grid-cols-6 col-span-3 '>
-    <div className=' flex flex-col items-start justify-start  p-4 space-y-3 w-full bg-pink-500 col-span-3 md:col-span-4'>
+    <div className=' flex flex-col items-start justify-start  p-4 space-y-3 w-full col-span-3 sm:col-span-4'>
      <div className={`${jost.className} text-[#262626]  text-[10px] lg:text-md`}>Stylish woman in summer outfit isolated posing in fashion trend isolated</div>
      <div className={`${jost.className} text-[#555555] text-[11px] hidden xl:block `}>In this captivating scene, a stylish woman stands confidently against a pristine backdrop, capturing the essence of summer's allure. Bathed in the gentle sunlight, she emanates an air of effortless chic. Her outfit is a harmonious blend of contemporary trends and timeless elegance, perfectly attuned to the vibrant season.</div>
-     <div className={`${jost.className} text-[#555555] text-[9px] xl:hidden`}>In this captivating scene, a stylish woman stands confidently against a pristine backdrop, capturing the essence of summer's allure. Bathed in the gentle ....</div>
+     <div className={`${jost.className} text-[#555555] text-[9px] hidden sm:block`}>In this captivating scene, a stylish woman stands confidently against a pristine backdrop, capturing the essence of summer's allure. Bathed in the gentle sunlight, she emanates an air of effortless chic. Her outfit ....</div>
+     <div className={`${jost.className} text-[#555555] text-[9px] sm:hidden`}>In this captivating scene, a stylish woman stands confidently against a pristine backdrop, capturing the essence of summer's allure. Bathed in the gentle ....</div>
     <div className='w-full flex items-start justify-start' >  <span style={{fontWeight:200}} className={`  gap-x-3 bg-[#4172DC]  p-2  text-[9px]  rounded-xs text-white flex items-center justify-center`}>READ MORE <FaArrowRight /></span></div>
      
     </div>
@@ -22,7 +23,7 @@ const TopLeft = () => {
 
 
      <Image height={400} width={400} className='bg-white  hidden md:block  col-span-2 h-full w-full    object-center ' src="/a-w.png" alt="blog_main_image" />  
-     <Image height={400} width={400} className='bg-white hidden sm:block md:hidden col-span-2 h-full w-full    object-center ' src="/blog_2.png" alt="blog_main_image" />  
+     <Image height={500} width={500} className='bg-white hidden sm:block md:hidden col-span-2 h-full w-full    object-center ' src="/blog_2.png" alt="blog_main_image" />  
      <Image height={500} width={500} className='bg-red-800 sm:hidden  col-span-2 h-full w-full    object-center ' src="/a-w.png" alt="blog_main_image" />  
      {/* <div className='w-full h-full bg-green-600'>  */}
  {/* f */}
