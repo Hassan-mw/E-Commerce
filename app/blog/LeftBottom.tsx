e9dba595ef4dea84efab9745766dbb2ce1bc2124
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-09 00:33:20 +0500

    Updating Frontend Data

--- app/blog/LeftBottom.tsx
+++ app/blog/LeftBottom.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 const LeftBottom = () => {
   return (
-    <div>
-        
+    <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-3'>
+     
+     {/* 1 */}
+     <div className='w-full h-full flex items-center justify-center'>
+      <Image height={300} width={300} src="/bottomleft_iamge_1.png" alt='bottomleft_iamge_1'  />
+      <div className='flex flex-col items-start justify-start space-y-2'>
+       <div className={`  text-xs text-[#FF7A00]` }>Fashion</div>
+      </div>
+     </div>
+
+
     </div>
   )
 }
