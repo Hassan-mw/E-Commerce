2bb9760993849b2796252c51673db21b6c4f303f
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-09 01:56:39 +0500

    Updating Frontend Data

--- app/blog/TextData.tsx
+++ app/blog/TextData.tsx
@@ -1,4 +1,5 @@
 import { Jost } from 'next/font/google'
+import Image from 'next/image'
 import React from 'react'
 
 const jost=Jost({
@@ -9,18 +10,38 @@ const jost=Jost({
 
 const TextData = () => {
  
-  
+  const data=[
+    {id:1,title:'The Essence of Time Travel',data:'Vintage fashion allows us to transcend eras, stepping into the shoes of those who came before us. Each piece tells a story, offering a glimpse into the cultural and societal norms of its time. From the flapper dresses of the 1920s that embodied the spirit of rebellion to the tailored suits of the 1960s that epitomized sophistication, every era has its signature style waiting to be revived.'},
+    {id:2,title:'Quality Beyond Compare',data:'One of the hallmarks of vintage fashion is the unparalleled craftsmanship that went into creating garments that stood the test of time. In a world where fast fashion often prioritizes quantity over quality, vintage pieces remind us of an era when attention to detail and durability were paramount. The feel of a vintage silk dress or the weight of a hand-sewn coat is a testament to the dedication of artisans of yesteryears.'},
+    {id:3,title:'Eclectic Personal Style',data:'Embracing vintage fashion allows us to curate a wardrobe thats uniquely ours. Mixing and matching vintage pieces with contemporary items creates a personal style that defies categorization. From pairing a 1970s bohemian blouse with modern jeans to accessorizing a little black dress with vintage brooches, the possibilities are endless and the results are often strikingly original.Sustainable Chic'},
+    {id:4,title:'Sustainable Chic',data:'As the world embraces sustainability, vintage fashion takes center stage as a prime example of eco-conscious style. Choosing pre-loved clothing not only reduces the demand for new production but also breathes new life into garments that might otherwise be forgotten. Its a nod to slow fashion, a movement that encourages thoughtful consumption and reduces the impact on the environment.'},
+  ]
 
   return (
     <div className='w-full flex  items-center justify-center'> 
-    <div className='w-full max-w-screen-lg flex flex-col space-y-4 '>
+    <div className='w-full max-w-screen-lg flex flex-col space-y-8 '>
+      {/* text-data */}
+    <div className='w-full  flex flex-col space-y-4 '>
      <div className={`${jost.className}  text-2xl text-[#262626] `}>"Unveiling Timeless Elegance: Exploring the Allure of Vintage Fashion"</div>
   
-      <div className='flex flex-col items-start justify-start space-y-1 '>
-  
-      <div className={`${jost.className} text-sm text-[#262626]`}>The Essence of Time Travel</div>
-      <div style={{fontWeight:300}} className={`${jost.className} text-xs text-[#707377]`}>Vintage fashion allows us to transcend eras, stepping into the shoes of those who came before us. Each piece tells a story, offering a glimpse into the cultural and societal norms of its time. From the flapper dresses of the 1920s that embodied the spirit of rebellion to the tailored suits of the 1960s that epitomized sophistication, every era has its signature style waiting to be revived.</div>
+    {data.map((data,index)=>
+      <div key={data.id} className='flex flex-col items-start justify-start space-y-1 '>
+      <div className={`${jost.className} text-sm text-[#262626]`}>{data.title}</div>
+      <div style={{fontWeight:300}} className={`${jost.className} text-xs text-[#707377]`}>{data.data}</div>
       </div>
+     ) }
+    </div>
+    {/* images */}
+    <div className='w-full flex items-center justify-center gap-x-3'>
+   {/* Large_image */}
+     <Image height={400} width={400} className='w-full h-full' src="/blog_below_large_image.png" alt="blog_below_large_image" />  
+   
+   {/* 3_small_iamge */}
+   <div className='w-full max-w-[30%] flex flex-col gap-4'>
+   <Image height={400} width={400} className='w-full h-full' src="/a-w.png" alt="blog_main_image" />  
+   blog_below_small_image_1
+   </div>
+    </div>
     </div>
     </div>
   )
