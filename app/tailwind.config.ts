8e7dd0be7384405c1e8a21f1c4977382546d9c38
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-10 01:26:05 +0500

    Updating Frontend Data

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
-    screens: {
-      mdl: '900px',
-      
+    extend:{
+    width:{
+      mdl:'900px'
+    }
     },
     colors: {
       'blue': '#1fb6ff',
@@ -17,15 +18,7 @@ module.exports = {
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
     }
-  }
+  
+}
 }
