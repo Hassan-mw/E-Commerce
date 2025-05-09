fcc45ace4592f5a39e0847c3a5538a6c62a1269c
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-10 01:09:00 +0500

    Updating Frontend Data

--- app/blog/BelowData.tsx
+++ app/blog/BelowData.tsx
@@ -43,11 +43,11 @@ import LeftBottom from './LeftBottom'
 
 const BelowData = () => {
   return (
-    <div className='w-full max-w-[1150px]  flex items-center justify-center lg:px-8 '>
+    <div className='w-full max-w-[1100px]  flex items-center justify-center lg:px-8 '>
 
 
   
-  <div className='w-full flex items-start justify-start bg-red-400  lg:space-x-12'>
+  <div className='w-full flex items-start justify-start   lg:space-x-12'>
   <div className='w-full flex flex-col space-y-4 items-center justify-between  '>
      <LeftTop/>
      <LeftBottom/>
