cd37a61c34337aa7041f2a2fb7eed7b97682e06a
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-10 15:34:15 +0500

    Updating Frontend Data

--- app/aboutus/AboutUsData.tsx
+++ app/aboutus/AboutUsData.tsx
@@ -25,6 +25,7 @@ const AboutUsData = () => {
     <div style={{fontWeight:100}} className={`${jost.className} text-lg text-[#262626]`}>Let's get to know luminae</div>
     <div style={{fontWeight:500}} className={` text-4xl text-[#262626] `}>Providing the best experience to make your <span className='text-blue-500'>Online Shopping</span></div>
     <div style={{fontWeight:200}} className={`${jost.className} text-md   text-[#555555]`}>At Luminae, we are more than just an online store – we are your ultimate destination for an unparalleled shopping experience. Our journey began with a simple yet powerful idea: to create a platform that not only offers a wide array of products but also fosters a sense of community and connection among our customers.</div>
+     
      {/* Numbers */}
      <div className='flex items-center justify-between  w-[70%]'>
 
@@ -69,15 +70,113 @@ const AboutUsData = () => {
 
 
 
+   {/* ms-screen-contant */}
+    <div className='hidden sm:block w-full'>
+    <div className='max-w-[850px] lg:max-w-full w-full flex items-center justify-center  '>
+    <div className=' w-full  grid grid-cols-2 items-start justify-start  gap-x-3 h-full  '>
 
-    <div className='w-full grid grid-cols-1 sm:grid-cols-2 items-end justify-end  gap-x-3 h-full '>
-       {/* Image */}
-     <div className='w-full pt-8 bg-green-500 h-full flex items-center justify-end'>
-     <Image height={500} width={500} className=' h-full max-h-[500px] w-full flex items-center object-contain sm:hidden ' src="/aboutus_main_image.png" alt="aboutus_main_image"/>
-     <Image height={500} width={500} className='  max-w-full object-contain hidden sm:block' src="/about_second_image.png" alt="about_second_image"/>
+    {/* text */}
+    <div className='w-full h-full   flex flex-col items-start justify-between md:pb-12 space-y-8 lg:space-y-14  '>
+
+    <div className='w-full flex flex-col  items-start justify-start space-y-3'>
+    <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#262626]`}>Know our service</div>
+    <div style={{fontWeight:500}} className={`   text-[#262626] text-3xl`}>We offer best service that will  <span className='text-blue-500'>make it easier</span></div>
+    <div style={{fontWeight:200}} className={`${jost.className} text-sm  lg:text-xl text-[#555555]`}> Discover unparalleled convenience with our top-tier service, designed to make your shopping experience smoother than ever.Experience shopping made effortless through our exceptional service that puts your needs at the forefront.Elevate your shopping journey with our unmatched service, redefining convenience and satisfaction.</div>
+     </div>
+
+    {/* 4 -boxes */}
+    <div className=' pb-12 lg:pb-20 w-full grid grid-cols-1 lg:grid-cols-2  place-content-center  gap-4 '>
+
+      {/*  1   */}
+    <div className='flex flex-col items-center justify-center shadow-2xl rounded-md border border-slate-100 bg-white p-2 space-y-4'>
+      <div className='flex items-start justify-start lg:flex-col lg:space-y-2 lg:items-center lg:justify-center'>
+    <span className='size-9 rounded-full bg-[#EAF3FB] flex  items-center justify-center text-blue-500 mr-4'><BsShop size={25} /></span>
+     <div className='flex flex-col items-start justify-start lg:space-y-2 lg:items-center lg:justify-center'>
+
+    <div style={{fontWeight:300}} className={`${jost.className}  text-[11px]`}>Full category shop</div>
+    <div style={{fontWeight:300}} className={`${jost.className}  text-[9px] text-[#555555]`}>Explore our comprehensive online store where you'll find a diverse range of products across multiple categories, all curated to cater to your various needs and ...</div>
+     </div>
+      </div>
+    <div className='w-full flex items-center justify-center p-2 bg-blue-500 rounded-xs '>
+      <span className={`${jost.className} text-[9px] gap-x-3  text-white flex items-center justify-center`}>READ MORE <FaArrowRight /></span>
+    </div>
+    </div>
+
+      {/*  2   */}
+    <div className='flex flex-col items-center justify-center shadow-2xl rounded-md border border-slate-100 bg-white p-2 space-y-4'>
+      <div className='flex items-start justify-start lg:flex-col lg:space-y-2 lg:items-center lg:justify-center'>
+    <span className='size-9 rounded-full bg-[#EAF3FB] flex  items-center justify-center text-blue-500 mr-4 '><RiDiscountPercentFill size={25} /></span>
+     <div className='flex flex-col items-start justify-start lg:space-y-2 lg:items-center lg:justify-center'>
+
+    <div style={{fontWeight:300}} className={`${jost.className}  text-[11px]`}>Extraordinary discount</div>
+    <div style={{fontWeight:300}} className={`${jost.className}  text-[9px] text-[#555555]`}> Experience unparalleled savings on a wide selection of premium products that enhance your lifestyle without compromising on quality...</div>
+     </div>
+      </div>
+    <div className='w-full flex items-center justify-center p-2 bg-blue-500 rounded-xs '>
+      <span className={`${jost.className} text-[9px] gap-x-3  text-white flex items-center justify-center`}>READ MORE <FaArrowRight /></span>
+    </div>
+    </div>
+
+
+      {/*  3  */}
+    <div className='flex flex-col items-center justify-center shadow-2xl rounded-md border border-slate-100 bg-white p-2 space-y-4'>
+      <div className='flex items-start justify-start lg:flex-col lg:space-y-2 lg:items-center lg:justify-center '>
+    <span className='size-9 rounded-full bg-[#EAF3FB] flex  items-center justify-center text-blue-500 mr-4'><RiCustomerService2Line size={25} /></span>
+     <div className='flex flex-col items-start justify-start lg:space-y-2  lg:items-center lg:justify-center'>
+
+    <div style={{fontWeight:300}} className={`${jost.className}  text-[11px]`}>24-hour customer service</div>
+    <div style={{fontWeight:300}} className={`${jost.className}  text-[9px] text-[#555555]`}>Enjoy the convenience of free cargo services, ensuring your purchases are delivered right to your doorstep without any additional cost. Experience seamless...</div>
+     </div>
+      </div>
+    <div className='w-full flex items-center justify-center p-2 bg-blue-500 rounded-xs '>
+      <span className={`${jost.className} text-[9px] gap-x-3  text-white flex items-center justify-center`}>READ MORE <FaArrowRight /></span>
+    </div>
+    </div>
+
+
+      {/*  4  */}
+    <div className='flex flex-col items-center justify-center shadow-2xl rounded-md border border-slate-100 bg-white p-2 space-y-4'>
+      <div className='flex items-start justify-start lg:flex-col lg:space-y-2 lg:items-center lg:justify-center '>
+    <span className='size-9 rounded-full bg-[#EAF3FB] flex  items-center justify-center text-blue-500 mr-4'><GiCargoShip size={25} /></span>
+     <div className='flex flex-col items-start justify-start lg:space-y-2 lg:items-center lg:justify-center'>
+
+    <div style={{fontWeight:300}} className={`${jost.className}  text-[11px]`}>Free Cargo</div>
+    <div style={{fontWeight:300}} className={`${jost.className}  text-[9px] text-[#555555]`}>Our commitment to exceptional customer care means our 24-hour customer service team is always available to assist you, ensuring your inquiries and concerns...</div>
+     </div>
+      </div>
+    <div className='w-full flex items-center justify-center p-2 bg-blue-500 rounded-xs '>
+      <span className={`${jost.className} text-[9px] gap-x-3  text-white flex items-center justify-center`}>READ MORE <FaArrowRight /></span>
+    </div>
+    </div>
+
+    
+    </div> 
+    
+     </div>
+
+     {/* Image */}
+     <div className='w-full  h-full flex items-end justify-end '>
+     <Image height={500} width={500} className='lg:hidden max-h-[900px] object-top-right object-contain w-full' src="/about_second_image.png" alt="about_second_image"/>
+     <Image height={500} width={500} className='hidden  lg:block max-h-[900px] object-contain  w-full' src="/aboutus_main_image.png" alt="aboutus_main_image"/>
      </div> 
-      {/* Context */}
-    <div className='w-full flex flex-col  items-start justify-start space-y-14'>
+   </div>
+   </div>
+   </div>
+
+
+
+
+
+
+
+    {/*mobile-screen- Context */}
+    <div className='sm:hidden w-full grid grid-cols-1 sm:grid-cols-2 items-start justify-start  gap-x-3 h-full '>
+     
+     <div className='w-full   h-full flex items-start justify-start'>
+     <Image height={500} width={500} className=' max-h-[600px]  object-cover object-top rounded-l-xl w-full' src="/aboutus_main_image.png" alt="aboutus_main_image"/>
+     </div> 
+     
+    <div className='sm:hidden w-full flex flex-col  items-start justify-start space-y-14'>
     <div className='w-full flex flex-col  items-start justify-start space-y-3'>
     <div style={{fontWeight:300}} className={`${jost.className} text-lg text-[#262626]`}>Know our service</div>
     <div style={{fontWeight:500}} className={`  text-4xl text-[#262626]`}>We offer best service that will  <span className='text-blue-500'>make it easier</span></div>
@@ -134,11 +233,12 @@ Elevate your shopping journey with our unmatched service, redefining convenience
 
     </div> 
      </div>
+     </div>
 
   
 
 
-    </div>
+  
        
     </div>
   )
