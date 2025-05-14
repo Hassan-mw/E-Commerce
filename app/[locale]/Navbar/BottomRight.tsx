f140872778cea2920836cd03c1d654821c772e35
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-15 01:54:41 +0500

    Updating Frontend Data

--- app/[locale]/Navbar/BottomRight.tsx
+++ app/[locale]/Navbar/BottomRight.tsx
@@ -10,13 +10,14 @@ const jost=Jost({
 import { FaCartArrowDown, FaFacebookF, FaTwitter } from "react-icons/fa6";
 import { GoHeart } from "react-icons/go";
 import { MdPerson } from "react-icons/md";
+import Link from 'next/link';
 
 const BottomRight = () => {
   return (
     <div className='w-full flex items-center justify-end gap-x-6 xl:gap-x-12 text-white'>
     <div className='flex items-center justify-center gap-x-2 hover:cursor-pointer'><MdPerson /><span>Sign in</span></div>
     <div className='flex items-center justify-center gap-x-2 hover:cursor-pointer'><GoHeart /> <span>Favourites</span></div>
-    <div className='flex items-center justify-center gap-x-2 hover:cursor-pointer'><FaCartArrowDown  /><span>Card</span></div>
+    <Link href='/cart' className='flex items-center justify-center gap-x-2 hover:cursor-pointer'><FaCartArrowDown  /><span>Card</span></Link>
 </div>
   )
 }
--- app/backend/Controller/cartController.js
+++ app/backend/Controller/cartController.js
@@ -1,3 +1,5 @@
+const AppError = require('../ErrorHandler/appError');
+const Pool=require('../Pool/pool');
 
 exports.getAllCart=async(req,resizeBy,next)=>{
     try{
@@ -12,9 +14,20 @@ exports.getAllCart=async(req,resizeBy,next)=>{
 
 exports.createCart=async(req,res,next)=>{
     try{
-
+        const {product_id}=req.body
+        console.log(product_id)
+        if(!product_id){
+            return next(new AppError('Can not find "product_id"',500))
+     
+        }
+    const {rows}=await Pool.query('INSERT INTO carts (product_id) VALUES ($1) RETURNING *',[product_id])
+     res.status(401).json({
+        staus:'success',
+        data:rows[0]
+     })
+  
     }catch(error){
-        resizeBy.status(500).json({
+        res.status(500).json({
             status:'fail',
             message:error.message
         })
