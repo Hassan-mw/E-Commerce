44c37f6a6a67a49df11695c25b9d749617f2c62f
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-15 02:13:22 +0500

    Updating Frontend Data

--- app/[locale]/cart/CartNavbar.tsx
+++ app/[locale]/cart/CartNavbar.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import Cart from './CartSections/Cart'
+import CustomerInfo from './CartSections/CustomerInfo'
+import ShippingPayment from './CartSections/ShippingPayment'
+import ProductConfirmation from './CartSections/ProductConfirmation'
 
 const CartNavbar = () => {
-  return (
-    <div>
+
+  const [currentDetails,setCurrentDetails]=useState('Cart')
+            const data=[
+                {id:1,name:'Cart'},
+                {id:2,name:'CustomerInfo'},
+                {id:3,name:'SHIPPING & PAYMENT'},
+                {id:4,name:'ProductConfirmation'}
+            ]
+
+
+          return (
+            <div className="w-full flex flex-col space-y-3 items-center justify-center gap-x-6 py-6 ">
+            <div className="w-full flex  items-center justify-center gap-x-6 ">
+                {
+                    data.map((data,index)=>
+                        <div key={data.id} onClick={()=>setCurrentDetails(data.name)} style={{fontWeight:400}} className={`${jost.className} ${currentDetails===data.name ? 'text-[#262626]' : 'text-[#9D9D9D]'  } hover:text-blue-500 duration-300 hover:cursor-pointer text-sm ${jost.className} `}>{data.name}</div>
+                    )
+                    }
+            </div>
         
-    </div>
+            {currentDetails==='CART' && <Cart />}
+            {currentDetails==='CustomerInfo' && <CustomerInfo/>}
+            {currentDetails==='SHIPPING & PAYMENT' && <ShippingPayment/>}
+            {currentDetails==='ProductConfirmation' && <ProductConfirmation/>}
+            </div>
+  
   )
 }
 
---
+++ app/[locale]/cart/CartSections/ProductConfirmation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductConfirmation = () => {
+  return (
+    <div>ProductConfirmation</div>
+  )
+}
+
+export default ProductConfirmation
