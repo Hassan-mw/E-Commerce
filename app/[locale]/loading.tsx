030c70edb8586192d4cb561169ed6ff2280bfa6b
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-05-30 22:48:03 +0500

    Updating Frontend Data

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div>loading...</div>
+    <div className='w-screen h-screen flex items-center justify-center'>
+     <div className='loader'/>
+    </div>
   )
 }
 
