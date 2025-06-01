46de118e8754c56477d4b4744d8bca7f320aabb1
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-06-01 16:34:15 +0500

    Updating Frontend Data

--- app/[locale]/Navbar/SmallScreen.tsx
+++ app/[locale]/Navbar/SmallScreen.tsx
@@ -42,11 +42,14 @@ const SmallScreen = () => {
      <Sidebar/>
 
     <Link href="/" className='flex items-center justify-center gap-x-3'><Image height={45} width={45} src="/Navbar_logo.png" alt="logo" /><span className={`${jost.className} text-2xl`}>Limunea</span></Link>
+    
     <div className='flex items-center justify-center gap-x-4'>
     <Link href="/cart" className='flex items-center justify-center gap-x-2 '><span className='hover:text-blue-400 duration-500'><FaCartArrowDown size={18}  /></span><span className='text-xl  hidden md:block'>Card</span></Link>
     <Link href='/order' className='flex items-center justify-center gap-x-2 hover:cursor-pointer'><span className='hover:text-blue-400 duration-500'><TfiPackage /></span><span  className="hidden md:block">Order</span>  </Link>
     </div>
+
     </div>
+
     <div className='w-full sm:w-[70%] flex items-center justify-center border gap-x-0 border-slate-200 rounded-sm py-1 px-2 '>
       <div className='w-full '><input type='text' className='w-5/6   focus:outline-none placeholder:text-slate-500 placeholder:text-sm ' placeholder='Search Product'/></div>
      <div className='mr-5 '>
@@ -70,6 +73,8 @@ const SmallScreen = () => {
     <FiSearch size={20} />
     </div>
     </div>
+
+    <Link href="/login">Login</Link>
     </div>
     </div>
   )
