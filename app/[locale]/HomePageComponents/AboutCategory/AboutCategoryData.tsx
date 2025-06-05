81938e744f1128241e49124a09231c3612ecf1a9
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-06-05 23:56:58 +0500

    Updating Frontend Data

--- app/[locale]/HomePageComponents/AboutCategory/AboutCategoryData.tsx
+++ app/[locale]/HomePageComponents/AboutCategory/AboutCategoryData.tsx
@@ -17,21 +17,22 @@ const AboutCategoryData = ({data}:AboutCategoryDataTYpe) => {
   return (
     <div className=' w-full flex items-center justify-center'>
     <div className='w-full  max-w-screen-xl grid grid-cols-1 sm:grid-cols-2 gap-6'>
-    {data.map((data,index:number)=>
+    {data.map((data,index)=>
    
      <div key={data.id} className='flex max-h-[350px] items-center justify-center w-full h-full rounded-md '>
    
-      <div className={`w-[70%] h-full flex flex-col space-y-6 sm:space-y-7 pb-12 items-start justify-between p-3 rounded-l-md ${data.style} text-white`}>
+     <div className={`w-[70%] h-full flex flex-col space-y-6 sm:space-y-7 pb-12 items-start justify-between p-3 rounded-l-md ${data.style} text-white`}>
      <div className='flex flex-col items-start justify-start space-y-3 lg:space-y-9'>
-
-     <div className={`${jost.className} text-xl sm:text-sm md:text-xl lg:text-3xl`}>{data.title}</div>
-     <div style={{fontWeight:300}} className={`${jost.className} text-md sm:text-xs lg:text-lg`}>{data.belowTitle}</div>
+     <h2 className={`${jost.className} text-xl sm:text-sm md:text-xl lg:text-3xl`}>{data.title}</h2>
+     <span style={{fontWeight:300}} className={`${jost.className} text-md sm:text-xs lg:text-lg`}>{data.belowTitle}</span>
      </div>
      <Link href={`/products/${data.color}`} style={{fontWeight:200}} className={`${jost.className} hover:cursor-pointer text-xs  sm:text-[10px] md:text-md lg:text-lg border-b border-white`}>Explore all category</Link>
-      </div>
-      <div className='w-full h-full lg:w-[70%] '>
-   <Image height={666} width={666} className='object-cover  max-h-[350px] h-full w-full object-center rounded-r-md' src={`/${data.image}`} alt='red-dress'/>
-      </div>
+    </div>
+
+    <div className='w-full h-full lg:w-[70%] '>
+    <Image height={666} width={666} className='object-cover  max-h-[350px] h-full w-full object-center rounded-r-md' src={`/${data.image}`} alt='red-dress'/>
+    </div>
+
      </div>
       )}
      
--- app/[locale]/HomePageComponents/AboutCategory/ScrollProductShow.tsx
+++ app/[locale]/HomePageComponents/AboutCategory/ScrollProductShow.tsx
@@ -15,6 +15,7 @@ const jost=Jost({
     CarouselNext,
     CarouselPrevious,
   } from "@/components/ui/carousel"
+import Link from 'next/link'
 
   
 const ScrollProductShow = () => {
@@ -22,29 +23,52 @@ const ScrollProductShow = () => {
     // <div className='w-full flex items-center justify-between px-20 bg-[#F7DDD0] rounded-md'>
       <Carousel className=' px-14'>
   <CarouselContent className=' max-h-80  gap-x-4 rounded-md max-w-screen-xl px-16 xl:px-2'>
+
+
     {/* //!  1 */}
     <CarouselItem  className=' w-full flex items-center justify-between px-20 xl:px-0 bg-[#ffffff] py-3 rounded-md '>
-     <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
+     {/* Content */}
+    <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
     <div className={`${jost.className} text-2xl text-[#31373a]`}>MAGSAFE</div>
     <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>Snap on a magnatic case,wallet, or both.And get fater wireless charging.</div>
-     </div>
-     <div className='bg-white'>
-        <Image height={333} width={333} className='object-cover h-full w-full max-h-44' src="/family_dress_1.jpg" alt='Iphone'/>
-     </div>
-     </CarouselItem>
-    {/* //!  2 */}
+    <Link href="/productsDetails/24" className='border rounded-md px-2 bg-blue-500 border-blue-500  duration-700 hover:bg-white hover:text-blue-500 hover:border-blue-500 text-white'>Check Out</Link>
+    </div>
+     {/* Image */}
+    <div className='bg-white'>
+    <Image height={333} width={333} className='object-cover h-full w-full max-h-44' src="/family_dress_1.jpg" alt='Iphone'/>
+    </div>
+    </CarouselItem>
 
-    <CarouselItem  className=' w-full flex items-center justify-between px-20 bg-[#ffffff] py-3 rounded-md'>
+    
+     {/* //!  2 */}
+    <CarouselItem  className=' w-full flex items-center justify-between px-20 gap-x-3 bg-[#ffffff] py-3 rounded-md'>
      <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
-    <div className={`${jost.className} text-2xl text-[#31373a]`}>Truly Free</div>
-    <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>Transform your cleaning routine with 9 essential products: Everyday Cleaner.</div>
+    <div className={`${jost.className} text-2xl text-[#31373a]`}>CAROTE</div>
+    <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>CAROTE Nonstick Pots and Pans,Cookware Set 16pcs Kitchen Cooking Sets, Pot and Pan Non Stick w/Frying pan.</div>
+    <Link href="/productsDetails/23" className='border rounded-md px-2 bg-blue-500 border-blue-500  duration-700 hover:bg-white hover:text-blue-500 hover:border-blue-500 text-white'>Check Out</Link>
+     
      </div>
-     <div className='bg-white'>
-        <Image height={333} width={333} className='object-cover h-full w-full max-h-44' src="/family_dress_2.jpg" alt='Iphone'/>
+     <div className='bg-white '>
+        <Image height={333} width={333} className='object-contain h-full w-full max-h-44' src="/family_dress_4.jpg" alt='Iphone'/>
      </div>
      </CarouselItem>
     
+
     {/* //!  3 */}
+    <CarouselItem  className=' w-full flex items-center justify-between px-20 bg-[#ffffff] py-3 rounded-md'>
+    {/* Content */}
+    <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
+    <div className={`${jost.className} text-2xl text-[#31373a]`}>Truly Free</div>
+    <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>Transform your cleaning routine with 9 essential products: Everyday Cleaner.</div>
+    </div>
+    {/* Image */}
+    <div className='bg-white'>
+    <Image height={333} width={333} className='object-cover h-full w-full max-h-44' src="/family_dress_2.jpg" alt='Iphone'/>
+    </div>
+
+     </CarouselItem>
+    
+    {/* //!  4 */}
 
     <CarouselItem  className=' w-full flex items-center justify-between px-20 bg-[#ffffff] py-3 rounded-md'>
      <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
@@ -56,18 +80,8 @@ const ScrollProductShow = () => {
      </div>
      </CarouselItem>
     
-    {/* //!  4 */}
-
-    <CarouselItem  className=' w-full flex items-center justify-between px-20 gap-x-3 bg-[#ffffff] py-3 rounded-md'>
-     <div className=' flex flex-col items-start justify-start space-y-2 pl-10'>
-    <div className={`${jost.className} text-2xl text-[#31373a]`}>CAROTE</div>
-    <div style={{fontWeight:300}} className={`${jost.className} text-md text-[#465D6B]`}>CAROTE Nonstick Pots and Pans,Cookware Set 16pcs Kitchen Cooking Sets, Pot and Pan Non Stick w/Frying pan.</div>
-     </div>
-     <div className='bg-white '>
-        <Image height={333} width={333} className='object-contain h-full w-full max-h-44' src="/family_dress_4.jpg" alt='Iphone'/>
-     </div>
-     </CarouselItem>
-    
+     
+   
      </CarouselContent>
   <CarouselPrevious />
   <CarouselNext />
